e365f61f1332b1bcf2ffdf012e374f6d6b74d330
Nicholas Kumia <nicholas.kumia@gsa.gov>
2021-07-15 15:08:32 -0400

    conform encoding check to ckanext-spatial standards

--- ckanext/spatial/harvesters/waf.py
+++ ckanext/spatial/harvesters/waf.py
@@ -98,12 +98,8 @@ class WAFHarvester(SpatialHarvester, SingletonPlugin):
 
         url_to_modified_harvest = {} ## mapping of url to last_modified in harvest
         try:
-            if six.PY2:
-                for url, modified_date in _extract_waf(content,source_url,scraper):
-                    url_to_modified_harvest[url] = modified_date
-            else:
-                for url, modified_date in _extract_waf(content.decode('utf-8'),source_url,scraper):
-                    url_to_modified_harvest[url] = modified_date
+            for url, modified_date in _extract_waf(six.text_type(content),source_url,scraper):
+                url_to_modified_harvest[url] = modified_date
         except Exception as e:
             msg = 'Error extracting URLs from %s, error was %s' % (source_url, e)
             self._save_gather_error(msg,harvest_job)
