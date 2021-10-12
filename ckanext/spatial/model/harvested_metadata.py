ff290a070f8b2fe15a86b16a915d9495cc08739a
jbrown-xentity <jbrown@xentity.com>
2021-10-12 07:30:51 -0600

    Add encoding to parser

--- ckanext/spatial/model/harvested_metadata.py
+++ ckanext/spatial/model/harvested_metadata.py
@@ -37,7 +37,7 @@ class MappedXmlDocument(MappedXmlObject):
 
     def get_xml_tree(self):
         if self.xml_tree is None:
-            parser = etree.XMLParser(remove_blank_text=True)
+            parser = etree.XMLParser(remove_blank_text=True, encoding='utf-8')
             xml_str = six.ensure_str(self.xml_str)
             self.xml_tree = etree.fromstring(xml_str, parser=parser)
         return self.xml_tree
